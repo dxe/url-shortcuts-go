77f2beb61c209c722ecffb4d95d2984fa7f2151e
Jake Hobbs <jakehobbs@gmail.com>
2021-10-20 15:17:07 -0700

    add logout button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.min.css';
+import { Button } from 'react-bulma-components';
 
 // TODO: move to env
 //const ROOT_PATH = "http://localhost:8080"
@@ -26,28 +28,15 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p><a href={ ROOT_PATH + "/login" }>Log in</a></p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>{
-          shortcuts && shortcuts.map((s: any) => {
-            return <p>{s.Code} | {s.URL}</p>
-          })
-        }
-        </p>
-      </header>
+    <div className="main">
+      <a href={ ROOT_PATH + "/login" }><Button color="primary">Log in</Button></a>
+      <a href={ ROOT_PATH + "/logout" }><Button color="primary">Log out</Button></a>
+      <p>{
+        shortcuts && shortcuts.map((s: any) => {
+          return <p>{s.Code} | {s.URL}</p>
+        })
+      }
+      </p>
     </div>
   );
 }
