87ca9fbcb2a8c360fab60823b9071e559498b841
Jake Hobbs <jakehobbs@gmail.com>
2021-10-20 17:37:42 -0700

    remove logging

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,10 +10,7 @@ const ROOT_PATH = "https://shortcuts.dxe.io"
 const getShortcutsList = async () => {
     try {
         const resp = await fetch(ROOT_PATH + "/api/shortcuts/list")
-        console.log(resp)
-        const body = await resp.json()
-        console.log(body)
-        return body
+        return resp.json()
     }
     catch(e) {
         console.error("Failed to load shortcuts.")
