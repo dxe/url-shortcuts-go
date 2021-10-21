d91d83d14438a169b476545d0172884227dd39e0
Jake Hobbs <jakehobbs@gmail.com>
2021-10-21 14:59:52 -0700

    separate frontend & api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,19 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 import 'bulma/css/bulma.min.css';
-import { Button } from 'react-bulma-components';
+import {Button, Navbar} from 'react-bulma-components';
 
 // TODO: move to env
-//const ROOT_PATH = "http://localhost:8080"
-const ROOT_PATH = "https://shortcuts.dxe.io"
+//const ROOT_API_PATH = "http://localhost:8080"
+const ROOT_API_PATH = "https://shortcuts.dxe.io"
 
 const getShortcutsList = async () => {
     try {
-        const resp = await fetch(ROOT_PATH + "/api/shortcuts/list")
+        const resp = await fetch(ROOT_API_PATH + "/api/shortcuts/list", {
+            // method: 'GET', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            credentials: 'include', // include, *same-origin, omit
+        })
         return await resp.json()
     }
     catch(e) {
@@ -19,8 +23,10 @@ const getShortcutsList = async () => {
 
 function App() {
   const [shortcuts, setShortcuts] = useState([]);
+  const [navbarActive, setNavbarActive] = useState(false);
 
   useEffect(() => {
+    // load shortcuts from api
     (async () => {
       const shortcuts = await getShortcutsList()
       setShortcuts(shortcuts)
@@ -29,14 +35,45 @@ function App() {
 
   return (
     <div className="main">
-      <a href={ ROOT_PATH + "/login" }><Button color="primary">Log in</Button></a>
-      <a href={ ROOT_PATH + "/logout" }><Button color="primary">Log out</Button></a>
-      <p>{
+
+
+        <Navbar active={navbarActive}>
+            <Navbar.Brand>
+                <Navbar.Item href="#">
+                    <strong>DxE Shortcuts</strong>
+                </Navbar.Item>
+                <Navbar.Burger onClick={() => setNavbarActive(!navbarActive)}/>
+            </Navbar.Brand>
+            <Navbar.Menu>
+                <Navbar.Container>
+                    <Navbar.Item href="#">
+                        Shortcuts
+                    </Navbar.Item>
+                    <Navbar.Item href="#">
+                        Visits
+                    </Navbar.Item>
+                    <Navbar.Item href="#">
+                        Users
+                    </Navbar.Item>
+                </Navbar.Container>
+                <Navbar.Container align="right">
+                    <Navbar.Item href={ ROOT_API_PATH + "/logout" }>
+                        Log out
+                    </Navbar.Item>
+                </Navbar.Container>
+            </Navbar.Menu>
+        </Navbar>
+
+      <a href={ ROOT_API_PATH + "/login" }><Button color="primary">Log in</Button></a>
+
+      <div>
+      {
         shortcuts && shortcuts.map((s: any) => {
           return <p>{s.Code} | {s.URL}</p>
         })
       }
-      </p>
+      </div>
+
     </div>
   );
 }
