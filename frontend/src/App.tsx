1ad6b525c268c86662d11a759f8188b6ba752b74
jakehobbs <jakehobbs@gmail.com>
2021-10-22 14:41:17 -0700

    frontend work in progress

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,32 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 import 'bulma/css/bulma.min.css';
-import {Button, Navbar} from 'react-bulma-components';
+import {Button, Navbar, Heading, Section, Card, Content, Box} from 'react-bulma-components';
 
 // TODO: move to env
 const ROOT_API_PATH = "http://localhost:8080"
 //const ROOT_API_PATH = "https://shortcuts.dxe.io"
 
-const getShortcutsList = async () => {
-    try {
-        const resp = await fetch(ROOT_API_PATH + "/api/shortcuts/list", {
-            // method: 'GET', // *GET, POST, PUT, DELETE, etc.
-            mode: 'cors', // no-cors, *cors, same-origin
-            credentials: 'include', // include, *same-origin, omit
-        })
-        return await resp.json()
-    }
-    catch(e) {
-        console.error("Failed to load shortcuts.")
-    }
-}
-
 function App() {
   const [shortcuts, setShortcuts] = useState([]);
   const [navbarActive, setNavbarActive] = useState(false);
+  const [apiError, setApiError] = useState(false)
+
+  const getShortcutsList = async () => {
+    try {
+      const resp = await fetch(ROOT_API_PATH + "/api/shortcuts/list", {
+        // method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        credentials: 'include', // include, *same-origin, omit
+      })
+      return await resp.json()
+    }
+    catch(e) {
+      console.error("Failed to load shortcuts.")
+      // TODO: only show login button if status code is 4xx
+      setApiError(true)
+    }
+  }
 
   useEffect(() => {
     // load shortcuts from api
@@ -34,45 +37,65 @@ function App() {
   }, [])
 
   return (
-    <div className="main">
+    <div className="page-wrapper">
 
+      <Navbar colorVariant={"dark"} active={navbarActive}>
+          <Navbar.Brand>
+              <Navbar.Item href="#">
+                  <strong>DxE Shortcuts</strong>
+              </Navbar.Item>
+              <Navbar.Burger onClick={() => setNavbarActive(!navbarActive)}/>
+          </Navbar.Brand>
+          <Navbar.Menu>
+              <Navbar.Container>
+                  <Navbar.Item href="#">
+                      Shortcuts
+                  </Navbar.Item>
+                  <Navbar.Item href="#">
+                      Visits
+                  </Navbar.Item>
+                  <Navbar.Item href="#">
+                      Users
+                  </Navbar.Item>
+              </Navbar.Container>
+              <Navbar.Container align="right">
+                  <Navbar.Item href={ ROOT_API_PATH + "/logout" }>
+                      Log out
+                  </Navbar.Item>
+              </Navbar.Container>
+          </Navbar.Menu>
+      </Navbar>
 
-        <Navbar active={navbarActive}>
-            <Navbar.Brand>
-                <Navbar.Item href="#">
-                    <strong>DxE Shortcuts</strong>
-                </Navbar.Item>
-                <Navbar.Burger onClick={() => setNavbarActive(!navbarActive)}/>
-            </Navbar.Brand>
-            <Navbar.Menu>
-                <Navbar.Container>
-                    <Navbar.Item href="#">
-                        Shortcuts
-                    </Navbar.Item>
-                    <Navbar.Item href="#">
-                        Visits
-                    </Navbar.Item>
-                    <Navbar.Item href="#">
-                        Users
-                    </Navbar.Item>
-                </Navbar.Container>
-                <Navbar.Container align="right">
-                    <Navbar.Item href={ ROOT_API_PATH + "/logout" }>
-                        Log out
-                    </Navbar.Item>
-                </Navbar.Container>
-            </Navbar.Menu>
-        </Navbar>
+      {apiError ? (<Section><a href={ ROOT_API_PATH + "/login" }><Button color="primary">Log in</Button></a></Section>)
 
-      <a href={ ROOT_API_PATH + "/login" }><Button color="primary">Log in</Button></a>
+        : (
+          <div>
+            <Section>
+              <Heading>
+                Shortcuts
+              </Heading>
+              <Button color="">
+                Add new shortcut
+              </Button>
+            </Section>
 
-      <div>
-      {
-        shortcuts && shortcuts.map((s: any) => {
-          return <p>{s.Code} | {s.URL}</p>
-        })
+            <Section>
+              {
+                shortcuts && shortcuts.map((s: any) => (
+                  <Box style={{ maxWidth: 600, margin: 'auto', marginBottom: 10 }}>
+                    <Heading size={4}>{s.Code}</Heading>
+                    <Heading subtitle size={6}>{s.URL}</Heading>
+                    <Button color="">
+                      Edit
+                    </Button>
+                  </Box>
+                ))
+              }
+            </Section>
+          </div>
+        )
+
       }
-      </div>
 
     </div>
   );
